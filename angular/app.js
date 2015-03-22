c6f9d7cc2a7b61c0593e82304485a23f25c680be
bilw <william@shinyobjects.net>
2015-03-22 15:12:02 -0400

    routing working

--- angular/app.js
+++ angular/app.js
@@ -2,7 +2,7 @@ var portfolioApp = angular.module("portfolioSharingApp", ["ngRoute"]);
 
 portfolioApp.config(function ($routeProvider) {
     $routeProvider
-        .when("/portfolios",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolio_list_partial.html" })
-        .when("/",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolio_list_partial.html" })
-        .otherwise({ redirectTo: "/404_page", templateUrl: "app/partials/404.html" });
+        .when("/portfolios",  { controller: "PortfolioListController", templateUrl: "angular/partials/portfolio_list_partial.html" })
+        .when("/",  { controller: "PortfolioListController", templateUrl: "angular/partials/portfolio_list_partial.html" })
+        .otherwise({ redirectTo: "/404_page", templateUrl: "angular/partials/404.html" });
 });
