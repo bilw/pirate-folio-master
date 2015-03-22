118b30eb17f022605748b15e8b0b39dc31c83b5c
bilw <william@shinyobjects.net>
2015-03-22 16:18:32 -0400

    add 404 page

--- angular/app.js
+++ angular/app.js
@@ -4,5 +4,5 @@ portfolioApp.config(function ($routeProvider) {
     $routeProvider
         .when("/portfolios",  { controller: "PortfolioListController", templateUrl: "angular/partials/portfolio_list_partial.html" })
         .when("/",  { controller: "PortfolioListController", templateUrl: "angular/partials/portfolio_list_partial.html" })
-        .otherwise({ redirectTo: "/404_page", templateUrl: "angular/partials/404.html" });
+        .otherwise({ redirectTo: "/404_page", templateUrl: "angular/partials/404_partial.html" });
 });
