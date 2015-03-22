82f4a540739dc7782b016c416e49cb85d0f5224e
bilw <william@shinyobjects.net>
2015-03-22 14:26:15 -0400

    initial commit to routing

---
+++ angular/app.js
@@ -0,0 +1,8 @@
+var portfolioApp = angular.module("portfolioSharingApp", ["ngRoute"]);
+
+portfolioApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/portfolios",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolio_list_partial.html" })
+        .when("/",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolio_list_partial.html" })
+        .otherwise({ redirectTo: "/404_page", templateUrl: "app/partials/404.html" });
+});
